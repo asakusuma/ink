a4d3e259e473f489a16b3b5ce0fa44b87625437c
asakusuma <asa.kusuma@gmail.com>
2014-05-28 09:00:06 -0700

    Move to jack

---
+++ jack.js
@@ -0,0 +1,48 @@
+var Store = {
+  asa: {
+      name: 'Asa',
+      age: 24
+  }
+};
+
+var Removers = {};
+
+var Jack = {
+    config: {
+      0: {
+        storage: 'memory',
+        life: 1000
+      },
+      1: {
+        storage: 'session',
+        life: 5000
+      }
+    },
+    get: function(key) {
+        if(Store.hasOwnProperty(key)){
+          return Store[key];
+        } else {
+          return null;
+        }
+    },
+    set: function(key, value, type) {
+      type = type || 0;
+      var config = this.config[type];
+      Store[key] = value;
+      this.setupCleanup(key, config);
+    },
+    setupCleanup: function(key, config) {
+      if(Removers[key]) {
+        clearTimeout(Removers[key].id);
+      }
+      var remove = function() {
+        delete Store[key];
+        delete Removers[key];
+      };
+      var id = setTimeout(remove, config.life);
+      Removers[key] = {
+        id: id,
+        callback: remove
+      };
+    }
+};
